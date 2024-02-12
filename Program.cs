a1bb181e4107291a2ad1e9f48f01e0fbc5fa2457
END <khoadeptrai07@gmail.com>
2024-02-12 23:45:28 +0700

    Initialization project

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Test
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World 2024!");
+        }
+    }
+}
