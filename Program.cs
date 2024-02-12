25a00ca0fc28a90d28a23c976beb5c0c65f41892
END <khoadeptrai07@gmail.com>
2024-02-13 00:08:52 +0700

    Hello World

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World 2024!");
+            Console.WriteLine("Hello World");
         }
     }
 }
